69deeb4ab02a472698873e4c3643444f699ce100
kmingseo <ms7happy@naver.com>
2025-01-20 13:17:39 +0900

    dp.1463

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[C++] BOJ_1463 1로 만들기",
-        "excerpt":"just for test test     ","categories": ["algorithm"],
+        "excerpt":"문제: 백준 1463 (c++) 풀이과정 우선 이 문제를 처음 봤을 때 이미 dp라는 것을 알고 접근했다. 그러나 아무리 생각해 봐도 도저히 10이 있을 때 바로 2로 나누는 것이 아닌 1을 먼저 뺌으로써 10이 1로 되기까지의 연산 횟수의 최솟값을 구하는 방법을 생각하는 데 어려움을 겪었다. 구글링을 통해 정보를 얻고 난 후에야...","categories": ["algorithm"],
         "tags": ["dp"],
         "url": "/algorithm/1463/",
         "teaser": null
