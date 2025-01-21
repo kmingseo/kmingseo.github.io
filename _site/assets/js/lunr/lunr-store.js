524e710d53b9acd4d1d832d09e126c7ee4e0adb6
kmingseo <ms7happy@naver.com>
2025-01-21 15:40:58 +0900

    dp.2579

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,13 @@
 var store = [{
-        "title": "just for test",
-        "excerpt":"just for test  ","categories": ["ML"],
-        "tags": ["test"],
-        "url": "/ml/test/",
-        "teaser": null
-      },{
-        "title": "just for test with category",
-        "excerpt":"Test for toc   for toc test  just for test  ","categories": ["algorithm"],
-        "tags": [],
-        "url": "/algorithm/test2/",
-        "teaser": null
-      },{
         "title": "[C++] BOJ_1463 1로 만들기",
         "excerpt":"문제: 백준 1463 (c++) 풀이과정 우선 이 문제를 처음 봤을 때 이미 dp라는 것을 알고 접근했다. 그러나 아무리 생각해 봐도 도저히 10이 있을 때 바로 2로 나누는 것이 아닌 1을 먼저 뺌으로써 10이 1로 되기까지의 연산 횟수의 최솟값을 구하는 방법을 생각하는 데 어려움을 겪었다. 구글링을 통해 정보를 얻고 난 후에야...","categories": ["algorithm"],
         "tags": ["dp"],
         "url": "/algorithm/1463/",
+        "teaser": null
+      },{
+        "title": "[C++] BOJ_2579 계단 오르기",
+        "excerpt":"문제: 백준 2579 (c++) 풀이과정 이 문제는 조건이 조금 까다롭다고 느낄 수 있다. 계단이 네 개가 있다고 가정을 했을 때, 계단 1, 2는 무조건 한 칸씩 가는 것이 최댓값을 구할 때 도움이 된다. 따라서 dp 배열에 먼저 값을 지정해 주었고, 세 번째 계단은 애초에 문제 조건에 연속 세 계단을 밟을...","categories": ["algorithm"],
+        "tags": ["dp"],
+        "url": "/algorithm/2579/",
         "teaser": null
       }]
